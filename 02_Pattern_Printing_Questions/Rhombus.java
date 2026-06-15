b1309591c5e3527b544d0416a59d0b774dc57f46
krishkumar2227 <krishkumar.200622@gmail.com>
2026-06-15 22:25:55 +0530

    Second Question

---
+++ 02_Pattern_Printing_Questions/Rhombus.java
@@ -0,0 +1,16 @@
+public class Rhombus {
+  public static void main(String[] args) {
+    int n = 5;
+    for(int row=1;row<=n;row++){
+     for(int col=1;col<=n-row;col++){
+      System.out.print(" ");
+     }
+    for(int j =1;j<=n;j++){
+      System.out.print("*");
+    }
+    System.out.println();
+  }
+    }
+  }
+
+
