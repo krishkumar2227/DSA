03b371481b92f30827aa8907e9e1e9c98c76db47
krishkumar2227 <krishkumar.200622@gmail.com>
2026-06-15 22:31:42 +0530

    Third Question

---
+++ 02_Pattern_Printing_Questions/Inverted_Right_Angled_Triangle.java
@@ -0,0 +1,12 @@
+public class Inverted_Right_Angled_Triangle {
+  public static void main(String[] args) {
+  int n = 5;
+    for(int row=1;row<=n;row++){
+     for(int col=1;col<=n-row;col++){
+      System.out.print(" ");
+      System.out.print("*");
+    }
+    System.out.println();
+  }
+    }
+  }
