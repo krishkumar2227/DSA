3d276181e3de2846957ca290b966095abb57f25e
krishkumar2227 <krishkumar.200622@gmail.com>
2026-06-16 07:55:59 +0530

    Fifth Question

--- 02_Pattern_Printing_Questions/Solid_Pyramid.java
+++ 02_Pattern_Printing_Questions/Solid_Pyramid.java
@@ -1,7 +1,7 @@
 public class Solid_Pyramid {
   public static void main(String[] args) {
       int n = 4;
-      for(int i = 1 ; i<=n ;i++){
+      for(int i =1; i<=n ;i--){
         for(int j = 1;j<=n-i;j++){
           System.out.print(" ");
         }
