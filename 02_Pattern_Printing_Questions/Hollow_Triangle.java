3a9ef54ee19a131ec4a9512b953e29686e0126bc
krishkumar2227 <krishkumar.200622@gmail.com>
2026-06-16 08:19:19 +0530

    Sixth Question

---
+++ 02_Pattern_Printing_Questions/Hollow_Triangle.java
@@ -0,0 +1,19 @@
+public class Hollow_Triangle {
+  public static void main(String[] args) {
+    int n = 5;
+    for(int i = 1 ;i<=n;i++){
+      if(i==1 || i==2 || i==5){
+        for(int j = 1;j<=i;j++){
+          System.out.print("*");
+        }
+      } else {
+        System.out.print("*");
+        for(int j =1;j<=i-2;j++){
+          System.out.print(" ");
+        }
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+}
