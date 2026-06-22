164ea4c398c6ec37e524a38aebd94d1f89bc9293
krishkumar2227 <krishkumar.200622@gmail.com>
2026-06-22 21:03:31 +0530

    Kadane's Algorithm

--- 03_Array_Questions/Q23_Maximum_Subarray.java
+++ 03_Array_Questions/Q23_Maximum_Subarray.java
@@ -1,7 +1,6 @@
 public class Q23_Maximum_Subarray { 
   static int maximumSubarray(int arr[]){
     int sum=0;
-    int n =arr.length;
     int maxSum=Integer.MIN_VALUE;
     for (int i = 0; i < arr.length;i++) {
         sum=sum+arr[i];
