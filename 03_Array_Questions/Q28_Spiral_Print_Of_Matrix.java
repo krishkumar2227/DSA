52fcc1f10a38c874f29a9dd7b0a6d5003954d1f1
krishkumar2227 <krishkumar.200622@gmail.com>
2026-06-24 23:14:28 +0530

    .

---
+++ 03_Array_Questions/Q28_Spiral_Print_Of_Matrix.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+public class Q28_Spiral_Print_Of_Matrix {
+  public List<Integer> spiralOrder(int [][] matrix){
+    int m = matrix.length;
+    int n = matrix[0].length;
+    List<Integer> result= new ArrayList<>();
+    int startingRow=0;
+    int endingRow=m-1;
+    int startingCol=0;
+    int endingCol=n-1;
+    while(startingRow<=endingRow && startingCol<=endingCol){
+      for(int col=startingCol;col<=endingCol;col++){
+        result.add(matrix[startingRow][col]);
+      }
+      startingRow++;
+      for(int row=startingRow;row<=endingRow;row++){
+        result.add(matrix[row][endingCol]);
+      }
+      endingCol--;
+      if(startingRow<=endingRow){
+      for(int col=endingCol;col>=startingCol;col--){
+      result.add(matrix[endingRow][col]);
+    }
+    endingRow--;
+  }
+    if(startingCol<=endingCol){
+    for(int row=endingRow;row>=startingRow;row--){
+      result.add(matrix[row][startingCol]);
+    }
+    startingCol++;
+  }
+}
+  return result;
+}
+public static void main(String[] args) {
+  int matrix[][]={{1,2,3},{4,5,6},{7,8,9}};
+    Q28_Spiral_Print_Of_Matrix obj = new Q28_Spiral_Print_Of_Matrix();
+
+    System.out.println(obj.spiralOrder(matrix));
+}
+}
