58d4b8dbc69d4bda1d18ba734fa60128e99521bf
krishkumar2227 <krishkumar.200622@gmail.com>
2026-06-25 20:48:38 +0530

    .

--- 03_Array_Questions/Q28_Spiral_Print_Of_Matrix.java
+++ 03_Array_Questions/Q28_Spiral_Print_Of_Matrix.java
@@ -4,6 +4,9 @@ public class Q28_Spiral_Print_Of_Matrix {
   public List<Integer> spiralOrder(int [][] matrix){
     int m = matrix.length;
     int n = matrix[0].length;
+    if(matrix == null || matrix.length == 0){
+    return new ArrayList<>();
+  }
     List<Integer> result= new ArrayList<>();
     int startingRow=0;
     int endingRow=m-1;
@@ -19,11 +22,11 @@ public class Q28_Spiral_Print_Of_Matrix {
       }
       endingCol--;
       if(startingRow<=endingRow){
-      for(int col=endingCol;col>=startingCol;col--){
-      result.add(matrix[endingRow][col]);
-    }
-    endingRow--;
-  }
+        for(int col=endingCol;col>=startingCol;col--){
+          result.add(matrix[endingRow][col]);
+      }
+      endingRow--;
+   }
     if(startingCol<=endingCol){
     for(int row=endingRow;row>=startingRow;row--){
       result.add(matrix[row][startingCol]);
