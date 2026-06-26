2d115e73a46acdb8209e55c8773a625100a6528e
krishkumar2227 <krishkumar.200622@gmail.com>
2026-06-26 22:23:32 +0530

    .

---
+++ 04_Searching/Number_Of_Occurence_Of_Target_Element.java
@@ -0,0 +1,44 @@
+//package 04_Searching;
+
+public class Number_Of_Occurence_Of_Target_Element {
+  static int getLowerBound(int arr[],int target){
+    int n = arr.length;
+    int s=0;
+    int e=n-1;
+    int ans=n;
+    while(s<=e){
+      int mid=s+(e-s)/2;
+      if(arr[mid]>=target){
+        ans=mid;
+        e=mid-1;
+      } else{
+        s=mid+1;
+      }
+    }
+    return ans;
+  }
+  static int getUpperBound(int arr[],int target){
+    int n = arr.length;
+    int s=0;
+    int e=n-1;
+    int ans=n;
+    while(s<=e){
+      int mid=s+(e-s)/2;
+      if(arr[mid]<=target){
+        s=mid+1;
+      }else{
+        ans=mid;
+        e=mid-1;
+      }
+  }
+  return ans;
+ }
+ public static void main(String[] args) {
+  int arr[]={10,20,20,20,20,20,30,3,40,40,50};
+  int target=20;
+  int lowerBound=getLowerBound(arr, target);
+  int upperBound=getUpperBound(arr, target);
+  int noOfOccurence=upperBound-lowerBound;
+  System.out.println(noOfOccurence);
+ }
+}
