946bfcbc45005ca08d92fe1ca8c0dabaad23b8ba
krishkumar2227 <krishkumar.200622@gmail.com>
2026-07-01 07:26:26 +0530

    Binary Search Used,Time Complexity(Ologn)

--- 04_Searching/Q06_Find_Square_Root_Of_X.java
+++ 04_Searching/Q06_Find_Square_Root_Of_X.java
@@ -2,7 +2,7 @@ public class Q06_Find_Square_Root_Of_X {
   static int squareRoot(int n){
     int s=0;
     int e=n;
-    int ans=-1;
+    double ans=0;
     if(n==0){
       return 0;
     }
@@ -17,6 +17,25 @@ public class Q06_Find_Square_Root_Of_X {
         s=mid+1;
       }
     }
-    return ans;
+    double factor=1;
+    int totalPrecision=3;
+    for(int round=1;round<=totalPrecision;round++){
+      factor=factor/10;
+      for(int i =0;i<10;i++){
+        double newAns=ans+factor;
+        if(newAns*newAns<n){
+          ans=newAns;
+        }else{
+          break;
+        }
+      }
+    }
+    System.out.println("Final Ans:"+Math.round(ans*1000.0)/1000.0);
+    return 1;
+  }
+  public static void main(String[] args) {
+    int n=56;
+    int ans=squareRoot(n);
+    System.out.println(ans);
   }
 }
