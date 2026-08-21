efc710664656a9520b3feb36268a2ede0d7692a5
krishkumar2227 <krishkumar.200622@gmail.com>
2026-08-21 23:50:02 +0530

    Time Complexity(O(n*m)) and Space Complexity(O(1))

---
+++ 03_Array_Questions/Q36_Toeplitz_Matrix.java
@@ -0,0 +1,20 @@
+public class Q36_Toeplitz_Matrix {
+    public static  boolean isToeplitzMatrix(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                 if(matrix[i][j]!=matrix[i-1][j-1]){
+                    return false;
+                 }
+            }
+        }
+    return true;
+  }
+  public static void main(String[] args) {
+    int [][] matrix= {{1,2,3,4},{5,1,2,3},{9,5,1,2}};
+    boolean ans= isToeplitzMatrix(matrix);
+    System.out.println(ans);
+  }
+}
+
