f13d9138ce524414ec8c7b46b2301fa9a48035c4
krishkumar2227 <krishkumar.200622@gmail.com>
2026-08-22 23:48:39 +0530

    .

--- 07_LinkedList/Q01_Singly_Linked_List.java
+++ 07_LinkedList/Q01_Singly_Linked_List.java
@@ -44,9 +44,56 @@ public class Q01_Singly_Linked_List {
     size++;
   }
   public void insertAtPosition(int position,int data){
-    
+    if(position < 1 || position>size+1){
+       System.out.println("Insertion Not possible.");
+       return ;
+    }
+    if(position == 1){
+      insertAtHead(data);
+      return ;
+    }
+    if(position == size+1){
+      insertAtTail(data);
+      return ;
+    }
+    Node prevNode=head;
+    for(int i=1;i<=position-2;i++){
+      prevNode=prevNode.next;
+    }
+
+    Node newNode=new Node(data);
+    newNode.next=prevNode.next;
+    prevNode.next=newNode;
+    size++;
+  }
+  public boolean isEmpty(){
+    return head==null;
+  }
+  public int getSize(){
+    return size;
+  }
+  public void printList(){
+    Node temp=head;
+    while(temp!=null){
+      System.out.print(temp.data+"->");
+      temp=temp.next;
+    }
+    System.out.println();
   }
   public static void main(String[] args) {
-    
+    Q01_Singly_Linked_List myList=new Q01_Singly_Linked_List();
+    if(myList.isEmpty()){
+      System.out.println("List is Empty.");
+    }
+    System.out.println("Size of LL:" +myList.getSize());
+
+    myList.insertAtHead(10);
+    myList.printList();
+    myList.insertAtHead(20);
+    myList.printList();
+    myList.insertAtHead(30);
+    myList.printList();
+    myList.insertAtHead(40);
+    myList.printList();
   }
 }
