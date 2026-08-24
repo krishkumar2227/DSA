30f1a27b505439117d89bea3c1facff110473ebb
krishkumar2227 <krishkumar.200622@gmail.com>
2026-08-24 14:27:02 +0530

    .

--- 07_LinkedList/Q01_Singly_Linked_List.java
+++ 07_LinkedList/Q01_Singly_Linked_List.java
@@ -20,6 +20,8 @@ public class Q01_Singly_Linked_List {
     this.tail=null;
     this.size=0;
   }
+
+  //insertAtHead:
   public void insertAtHead(int data){
     Node newNode=new Node(data);
 
@@ -32,6 +34,8 @@ public class Q01_Singly_Linked_List {
     }
     size++;
   }
+
+  //insertAtTail:
   public void insertAtTail(int data){
     Node newNode= new Node(data);
     if(head == null && tail==null){
@@ -43,6 +47,8 @@ public class Q01_Singly_Linked_List {
     }
     size++;
   }
+
+  //insertAtPosition:
   public void insertAtPosition(int position,int data){
     if(position < 1 || position>size+1){
        System.out.println("Insertion Not possible.");
@@ -66,6 +72,8 @@ public class Q01_Singly_Linked_List {
     prevNode.next=newNode;
     size++;
   }
+
+  //Utility Functions:
   public boolean isEmpty(){
     return head==null;
   }
@@ -80,6 +88,90 @@ public class Q01_Singly_Linked_List {
     }
     System.out.println();
   }
+  public int getHead(){
+    return head.data;
+  }
+  
+  //Traversal in linked list:
+  public void traversal(){
+    Node temp=head;
+    while(temp!=null){
+      System.out.println(temp.data);
+      temp=temp.next;
+    }
+  }
+
+  //Search in LinkedList:
+  public boolean search(int target){
+    Node temp=head;
+    while(temp!=null){
+      if(temp.data == target){
+        return true;
+      }else{
+        temp=temp.next;
+      }
+    }
+  return false;
+  }
+
+  //deleteAtHead:
+  public void deleteAtHead(){
+    if(head==null){
+       System.out.println("LL is empty cannot delete at head:");
+       return;
+    }else{
+      head=head.next;
+    }
+  }
+  
+  //deleteAtTail:
+  public void deleteAtTail(){
+    if(head==null){
+      System.out.println("LL is empty:");
+      return;
+    }
+    if(head==tail){
+      head=null;
+      tail=null;
+      size=0;
+      return;
+    }
+    Node temp=head;
+    for(int i =1;i<=size-2;i++){
+      temp=temp.next;
+    }
+    temp.next=null;
+    tail=temp;
+    size--;
+  }
+
+  //deleteAtPosition:
+  public void deleteAtPosition(int position){
+    if(position<1 || position>size+1){
+      System.out.println("Invalid Position");
+      return;
+    }
+    if(position ==1){
+      deleteAtHead();
+      return;
+    }
+    if(position == size+1){
+      deleteAtTail();
+      return ;
+    }
+    Node prev = head;
+    for(int i =1;i<=position-2;i++){
+      prev=prev.next;
+    }
+    Node curr= prev.next;
+    Node forward=curr.next;
+
+    prev.next=forward;
+    curr.next=null;
+
+    size--;
+  }
+
   public static void main(String[] args) {
     Q01_Singly_Linked_List myList=new Q01_Singly_Linked_List();
     if(myList.isEmpty()){
