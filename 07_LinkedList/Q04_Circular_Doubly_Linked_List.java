cf9766effc5d0da53070e06125c724db354161c5
krishkumar2227 <krishkumar.200622@gmail.com>
2026-08-27 23:58:13 +0530

    .

---
+++ 07_LinkedList/Q04_Circular_Doubly_Linked_List.java
@@ -0,0 +1,36 @@
+public class Q04_Circular_Doubly_Linked_List{
+  static class Node{
+    int data;
+    Node next;
+    Node prev;
+    //constructor
+    Node(int data){
+      this.data=data;
+      this.next=null;
+      this.prev=null;
+    }
+  }
+  private Node head;
+  private Node tail;
+  private int size;
+  
+   public Q04_Circular_Doubly_Linked_List(){
+    this.head=null;
+    this.tail=null;
+    this.size=0;
+  }
+  //insertAtHead:
+  public void insertAtHead(int data){
+  Node newNode=new Node(data);
+  if(head==null){
+    head=newNode;
+    tail=newNode;
+
+    head.prev=tail;
+    tail.next=head;
+  }else{
+     newNode.next=head;
+     newNode.prev=tail;
+  }
+}
+}
