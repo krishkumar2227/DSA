588c8b99b1e669b022b8e9ec9d806c75f590dca9
krishkumar2227 <krishkumar.200622@gmail.com>
2026-09-06 23:55:13 +0530

    Time Complexity(O(n)) and Space Complexity(O(1))

---
+++ 03_Array_Questions/Q39_Search_Insert_Position.java
@@ -0,0 +1,19 @@
+public class Q39_Search_Insert_Position {
+  public int searchInsert(int[] nums, int target) {
+        int n = nums.length;
+        int s=0;
+        int e=n-1;
+        
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(nums[mid]==target){
+                return mid;
+            }else if(nums[mid]<target){
+                s=mid+1;
+            } else{
+                e=mid-1;
+        }
+    }
+    return s;
+    }
+}
